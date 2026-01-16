35197fe4a599cfab407c4566f2c0f6dd05cef68b
CommanderBiz <s3nokh@gmail.com>
2026-01-16 13:06:04 -0600

    Added 'range' function to scan multiple open ports

--- scanner.py
+++ scanner.py
@@ -1,29 +1,37 @@
 import socket
 import sys
+from datetime import datetime
 
-def scan_port(ip, port):
-    print(f"[-] Scanning {ip} on port {port}...")
+
+def scan_target(ip, start_port, end_port):
+    print("-" * 50)
+    print(f"[-] Scanning Targer: {ip}....")
+    print(f"[-] Scan started at: {datetime.now()}")
+    print("-" * 50)
+
     
     try:
-        # Create a socket object
-        # AF_INET = Use IPv4
-        # SOCK_STREAM = Use TCP (The handshake protocol)
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        # Don't wait more than 1 second for a reply
-        s.settimeout(1)
-        
-        # connect_ex is like connect(), but returns an error code instead of crashing
-        # 0 means Success (The port is OPEN)
-        result = s.connect_ex((ip, port))
-        
-        if result == 0:
-            print(f"[+] SUCCESS: Port {port} is OPEN!")
-        else:
-            print(f"[!] Port {port} is CLOSED or FILTERED.")
+        # The loop! "range" goes up to, but does NOT include the end_port,so we add 1
+        for port in range(start_port, end_port + 1):
+            # Create a socket object
+            # AF_INET = Use IPv4
+            # SOCK_STREAM = Use TCP (The handshake protocol)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             
-        s.close()
-        
+            # Don't wait more than 1 second for a reply
+            s.settimeout(1)
+            
+            # connect_ex is like connect(), but returns an error code instead of crashing
+            # 0 means Success (The port is OPEN)
+            result = s.connect_ex((ip, port))
+            
+            if result == 0:
+                print(f"[+] SUCCESS: Port {port} is OPEN!")
+            else:
+                print(f"[!] Port {port} is CLOSED or FILTERED.")
+                
+            s.close()
+
     except KeyboardInterrupt:
         print("\n Exiting...")
         sys.exit()
@@ -31,11 +39,12 @@ def scan_port(ip, port):
         print("\n Could not connect to server.")
         sys.exit()
 
+       
 if __name__ == "__main__":
-    # We will scan "localhost" (127.0.0.1) which is your own machine
-    # Port 22 is SSH. Since you just used SSH to push to GitHub, 
-    # we KNOW this port should be open.
+    # We will scan "localhost" (127.0.0.1) which is your own machine 
     target_ip = "127.0.0.1" 
-    target_port = 22
     
-    scan_port(target_ip, target_port)
+    
+    # WARNING: Scanning 65535 ports takes a long time in Python. 
+    # Real hackers use tools like Nmap for speed, but this teaches you the logic.
+    scan_target(target_ip, 1, 1024)
